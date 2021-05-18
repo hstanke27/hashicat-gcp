20b8eecffd1f65ce8b036f07c2019a11509bb9b7
Holger Stanke <hstanke27@web.de>
2021-05-18 11:06:59 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 module "network" {
   source  = "app.terraform.io/holger-training/network/google"
   version = "2.5.0"
+  network_name = "gaurav-network"
   project_id = var.project
   subnets = [
   {
